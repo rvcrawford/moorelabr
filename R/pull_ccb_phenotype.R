e7495fb12fe880a5df16815a7beb894846055cd6
rvcrawford <rvcrawford@gmail.com>
2024-03-28 13:11:31 -0400

    corrected documentation for pull phenotype function

--- R/pull_ccb_phenotype.R
+++ R/pull_ccb_phenotype.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 #' my_dat = c("5,2023-05-15T14:04:44.55",  "7,2023-05-15T14:04:57.667")
-#' pull_ccb_date(my_dat)
+#' pull_ccb_phenotype(my_dat)
 pull_ccb_phenotype <- function(x){
 
   data.table::tstrsplit(x, ",", type.convert = T, keep = 1) |> unlist()
