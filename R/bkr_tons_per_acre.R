88a0d97aece8b2dd0dd2234031d08ed6bb10a497
rvcrawford <rvcrawford@gmail.com>
2024-03-28 08:04:41 -0400

    correct documentation for bkr yields function

--- R/bkr_tons_per_acre.R
+++ R/bkr_tons_per_acre.R
@@ -1,3 +1,14 @@
+#' Yields in tons per acre assuming 18 ft^2 plot
+#'
+#' @param x A number
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' bkr_tons_per_acre(c(2.25, 6))
 bkr_tons_per_acre <- function(x){
+
   x/18*43560/2000
-}
+
+  }
